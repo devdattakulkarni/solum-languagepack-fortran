31ab1660402e7bdf20c2f0c59ef0947b2998e4f1
Devdatta Kulkarni <devdatta.kulkarni@rackspace.com>
2015-07-19 17:28:12 -0500

    Added Dockerfile and build.sh

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:precise
+MAINTAINER Devdatta Kulkarni <devdatta.kulkarni@rackspace.com>
+
+RUN apt-get -yqq update
+RUN apt-get -yqq install gfortran, python-pip, python-dev
+
+COPY build.sh /solum/bin/
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if gfortran is installed
+gfortran --help | head -1 | grep 'Usage: gfortran \[options\] file...'
+[[ $? != 0 ]] && echo gfortran is not installed. && exit 1
+
+# Check if pip is installed
+pip help
+[[ $? != 0 ]] && echo python-pip is not installed. && exit 1
+
+# Install dependencies from app requirements file
+cd /app
+pip install -r requirements.txt; make clean; make
